33b965a23c7a19767ce52272861f0df2791af82f
DzmitryVM <mihalych411@tut.by>
2019-08-07 00:09:50 +0300

    Доработки и комменты

--- src/lection04/Task4_1.java
+++ src/lection04/Task4_1.java
@@ -2,32 +2,26 @@ package lection04;
 
 public class Task4_1 {
     public static void main(String[] args) {
-        TimeLength timeOne = new TimeLength("One", 12, 24, 32);
-        TimeLength timeTwo = new TimeLength("Two", 32, 78, 123);
-        TimeLength timeThree = new TimeLength("Three", 32000);
-        TimeLength timeFour = new TimeLength("Four", 32000);
+        TimePeriod timeFirst = new TimePeriod("First", 5, 53, 10);
+        TimePeriod timeSecond = new TimePeriod("Second", 33, 110, 95);
+        TimePeriod timeThird = new TimePeriod("Third", 21190);
+        TimePeriod timeFourth = new TimePeriod("Fourth", 35435);
 
-        printTimeLength(timeOne);
-        printTimeLength(timeTwo);
-        printTimeLength(timeThree);
-        printTimeLength(timeFour);
+        printTimePeriod(timeFirst);
+        printTimePeriod(timeSecond);
+        printTimePeriod(timeThird);
+        printTimePeriod(timeFourth);
 
-        System.out.println(timeOne.compareTo(timeTwo));
-        System.out.println(timeOne.compareTo(timeThree));
-        System.out.println(timeOne.compareTo(timeFour));
-        System.out.println(timeTwo.compareTo(timeThree));
-        System.out.println(timeTwo.compareTo(timeFour));
-        System.out.println(timeThree.compareTo(timeFour));
     }
 
-    public static void printTimeLength(TimeLength time) {
-        System.out.println("Name [" + time.getName() + "]:");
-        System.out.println("time =    " + time);
-        System.out.println("seconds = " + time.getShareSeconds());
+    public static void printTimePeriod(TimePeriod time) {               // вывод поочередно:
+        System.out.println("Name _" + time.getName() + "_:");           // имя объекта,
+        System.out.println("seconds = " + time.secondsQuantity());      // время, пересчитанное в секунды,
+        System.out.println("time =    " + time);                        // по отдельности часы, минуты, секунды
     }
 }
 
-class TimeLength {
+class TimePeriod {
     private String name;
     private int hours;
     private int minutes;
@@ -49,44 +43,33 @@ class TimeLength {
         return seconds;
     }
 
-    public TimeLength(String name, int hours, int minutes, int seconds) {
-        this.name = name;
-        this.hours = Math.abs(hours) % 24;
-        this.minutes = Math.abs(minutes) % 60;
-        this.seconds = Math.abs(seconds) % 60;
+    public TimePeriod(String name, int hours, int minutes, int seconds) {  // метод "отсечения превышающих значений"
+        this.name = name;                                      // для 1-го и 2-го объектов
+        this.hours = Math.abs(hours) % 24;                     // перевод в формат 24-часовых суток
+        this.minutes = Math.abs(minutes) % 60;                 // в формат 60мин в часе
+        this.seconds = Math.abs(seconds) % 60;                 // в формат 60сек в минуте
     }
 
-    public TimeLength(String name, int seconds) {
+    public TimePeriod(String name, int seconds) {               // метод для 3-го и 4-го объектов
         this.name = name;
         convertSecondsToTime(seconds);
     }
 
-    public int getShareSeconds() {
+    public int secondsQuantity() {                               // метод определения общего кол-ва секунд в объекте
         return this.hours * 3600 + this.minutes * 60 + seconds;
     }
 
-    private void convertSecondsToTime(int seconds) {
+    private void convertSecondsToTime(int seconds) {             // метод определения времени (час,мин,сек) из секунд
         seconds = Math.abs(seconds);
-        this.hours = seconds / 3600 % 24;
+        this.hours = seconds / 3600;                        // расчет часов и т.д.
         seconds %= 3600;
         this.minutes = seconds / 60;
         this.seconds = seconds % 60;
     }
 
     @Override
-    public String toString() {
+    public String toString() {                                   // выводим итог пересчета из секунд в час, мин,сек
         return String.format("%d h. %d m. %d s", this.hours, this.minutes, this.seconds);
     }
 
-    public String compareTo(TimeLength t) {
-        String result = "";
-        if (t != null) {
-            int thisObj = getShareSeconds();
-            int otherObj = t.getShareSeconds();
-            result = String.format("%s %s %s", getName(), thisObj == otherObj ? "==" : thisObj > otherObj ? ">" : "<", t.getName());
-        } else {
-            result = "Input object == null";
-        }
-        return result;
-    }
 }
--- src/lection04/Task4_2.java
+++ src/lection04/Task4_2.java
@@ -1,45 +1,58 @@
 package lection04;
 
 public class Task4_2 {
-    private static int x100;
-    private static int x50;
-    private static int x20;
+    private static int val20;
+    private static int val50;
+    private static int val100;
 
-    public static void addMoney(int addnote100, int addnote50, int addnote20) {
+    public static void addMoney(int addvalue20, int addvalue50, int addvalue100) {
         // запускаем класс Task4_2
 
-        addnote100 += x100;
-        addnote50 += x50;
-        addnote20 += x20;
-
+        val20 += addvalue20;
+        val50 += addvalue50;
+        val100 += addvalue100;
+        System.out.println("В банкомате в наличии " +  (addvalue20 * 20 + addvalue50 * 50 +addvalue100 * 100) + " руб.");
     }
 
     static void askMoney(int requestCash) {
-        if (requestCash % 10 != 0) {
-            System.out.println("Неверно введена сумма, пожалуйста, введите правильную сумму");
+        if (requestCash % 20 != 0) {
+            System.out.println("Неверно введена сумма! Пожалуйста, введите сумму, кратную 20");
         } else {
             boolean firstAnswer;
-            if (x100 * 100 + x50 * 50 + x20 * 20 >= requestCash) {
+            if (val20 * 20 + val50 * 50 + val100 * 100 >= requestCash) {
                 firstAnswer = true;
-                System.out.println("Успешно");
+                System.out.println("Запрос выполнен успешно");
             } else {
                 firstAnswer = false;
-                System.out.println("Недостаточно средств");
+                System.out.println("Недостаточно средств в банкомате");
             }
 
         }
 
     }
+
 }
 
 class Bankomat {
+    private int ask100;
+    private int ask50;
+    private int ask20;
 
     public static void main(String[] args) {
         Task4_2 CM1 = new Task4_2();
 
-        CM1.addMoney(50, 30, 25);
-        CM1.askMoney(100);
+        CM1.addMoney(50, 10, 25);
+        CM1.askMoney(1200);
 
     }
+            // дальше не работает
+    /*private void convertWhatValues(int requestCash) {             // метод определения видов выдаваемых купюр
+        requestCash = Math.abs(requestCash);
+        this.ask100 = requestCash / 100;                           // расчет
+        requestCash %= 100;
+        this.ask50 = requestCash / 50;
+        this.ask20 = requestCash % 20;
+        System.out.println(ask100);
+    }*/
 
 }
