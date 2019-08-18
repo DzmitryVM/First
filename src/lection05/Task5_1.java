063737ad900b29d0aa0c704cb38a1ed1f76cd9a3
DzmitryVM <mihalych411@tut.by>
2019-08-18 13:46:58 +0300

    Задание 5

--- src/lection05/Task5_1.java
+++ src/lection05/Task5_1.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class Task5_1 {
-    private static int num1=2;
+    private static int num1=1;
     private static int num2=6;
     private static String name = "Класс с двумя аргументами: ";
 
